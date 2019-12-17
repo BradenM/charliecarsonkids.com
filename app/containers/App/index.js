b8719407ecefd9334a72beb4b208099b9cbb9d51
Braden Mars <bradenmars@bradenmars.me>
2019-12-17 11:01:31 -0600

    :sparkles: (app) add bootstrap container to root component

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,22 +7,23 @@
  *
  */
 
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
-
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom';
 import GlobalStyle from '../../global-styles';
 
 export default function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-      <GlobalStyle />
-    </div>
+    <React.Fragment>
+      <Container>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+        <GlobalStyle />
+      </Container>
+    </React.Fragment>
   );
 }
