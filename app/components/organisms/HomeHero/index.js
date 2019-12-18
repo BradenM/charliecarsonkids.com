464ab7dfd489ec1622fb16e4b1fdca93a34a8823
Braden Mars <bradenmars@bradenmars.me>
2019-12-18 03:46:20 -0600

    :bug: (molecules) fix HomeHero min-height

--- app/components/organisms/HomeHero/index.js
+++ app/components/organisms/HomeHero/index.js
@@ -13,9 +13,9 @@ import { ImageURL } from 'utils/getStatic';
 
 const HeroContainer = styled(Container)`
   width: 100%;
-  min-height: 70vh;
   .hero {
     box-shadow: 0px 0px 20px 0px RGBA(39, 40, 43, 0.15);
+    min-height: 60vh;
   }
 `;
 
