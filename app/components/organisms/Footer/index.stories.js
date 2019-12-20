564462d4e4fa1cd6bf9120480213d5a37cd04ae0
Braden Mars <bradenmars@bradenmars.me>
2019-12-20 17:24:03 -0600

    :sparkles: (organisms) add Footer component

---
+++ app/components/organisms/Footer/index.stories.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * Stories for Footer
+ *
+ * Organism
+ */
+
+import React from 'react';
+import Footer from '.';
+
+export default {
+  title: 'Design|Organisms/Footer',
+  component: Footer,
+  parameters: {
+    centered: { disable: true },
+  },
+};
+
+export const defaultFooter = () => <Footer />;
