4796db28803b902519c4723a6d8be60136409131
Braden Mars <bradenmars@bradenmars.me>
2019-12-22 13:33:55 -0600

    test(homepage): test async render of homepage component

--- app/components/pages/HomePage/tests/index.test.js
+++ app/components/pages/HomePage/tests/index.test.js
@@ -9,6 +9,7 @@ import { render } from '@testing-library/react';
 // import '@testing-library/jest-dom/extend-expect'; // add some helpful assertions
 import { createStates, withRouter } from 'utils/testHelpers';
 import HomePage, { HomeBanner, HomeHero } from '../index';
+import AsyncHomePage from '../loadable';
 
 const testProps = {
   ...HomeBanner,
@@ -16,10 +17,24 @@ const testProps = {
 };
 
 describe('<HomePage />', () => {
+  let store;
+  let history;
+
+  beforeAll(() => {
+    const states = createStates();
+    store = states.store;
+    history = states.history;
+  });
+
   it('Expect to not log errors in console', () => {
     const spy = jest.spyOn(global.console, 'error');
-    const { store, history } = createStates();
     render(withRouter(HomePage, store, history, testProps));
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('Expect to nog log errors in console when loaded asynchronously', () => {
+    const spy = jest.spyOn(global.console, 'error');
+    render(withRouter(AsyncHomePage, store, history, testProps));
     expect(spy).not.toHaveBeenCalled();
   });
 });
