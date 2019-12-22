8ef099ff22ea7e67c5ccdb9f6bc4a198ea287e9b
Braden Mars <bradenmars@bradenmars.me>
2019-12-22 02:33:34 -0600

    test(storybook): fix font imports, update snapshots

--- .storybook/config.js
+++ .storybook/config.js
@@ -11,10 +11,14 @@ import { withKnobs } from '@storybook/addon-knobs/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { addDecorator, addParameters, configure } from '@storybook/react';
 import { withHTML } from '@whitespace/storybook-addon-html/react';
-// fonts
-import 'typeface-montserrat';
-import 'typeface-poppins';
-import 'typeface-roboto';
+/*
+ *  app fonts
+ *  jest requires 'index.css' for import to be mocked.
+ *  see @ https://github.com/facebook/jest/issues/8988
+ */
+import 'typeface-montserrat/index.css';
+import 'typeface-poppins/index.css';
+import 'typeface-roboto/index.css';
 // styles
 import '../app/style/custom.scss';
 import './storybook.css';
