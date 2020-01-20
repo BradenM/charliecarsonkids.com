aec450fd4e97916ea1be0528cba9e2fa06e5ee45
Braden Mars <bradenmars@bradenmars.me>
2020-01-20 04:41:25 -0600

    test: add storybook actions to PosterImage

--- app/components/molecules/PosterImage/index.stories.js
+++ app/components/molecules/PosterImage/index.stories.js
@@ -5,6 +5,7 @@
  * Molecules
  */
 
+import { withActions } from '@storybook/addon-actions';
 import React from 'react';
 // import { text, number, boolean } from '@storybook/addon-knobs';
 import { PlaceholderURL } from 'utils/getStatic';
@@ -13,6 +14,7 @@ import PosterImage from '.';
 export default {
   title: 'Design|Molecules/PosterImage',
   component: PosterImage,
+  decorators: [withActions('mouseenter', 'mouseleave')],
 };
 
 export const defaultStory = () => (
