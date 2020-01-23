e0f4e058786c3b74f344f773c719643e78476faa
Braden Mars <bradenmars@bradenmars.me>
2020-01-22 22:11:16 -0600

    feat: add arrow link transition UX

--- app/components/molecules/ArrowLink/index.js
+++ app/components/molecules/ArrowLink/index.js
@@ -7,13 +7,16 @@
 
 import Text from 'atoms/Text';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as S from './styles';
 
 const ArrowLink = ({ to, children, ...props }) => (
-  <S.Wrapper to={to}>
-    <Text variant="h6" {...props}>
-      {children}
-    </Text>
+  <S.Wrapper className="mx-auto">
+    <Link to={to}>
+      <Text variant="h6" {...props}>
+        {children}
+      </Text>
+    </Link>
     <S.Icon width="32" name="arrow-right" iconSize="50" />
   </S.Wrapper>
 );
--- app/components/molecules/ArrowLink/styles.js
+++ app/components/molecules/ArrowLink/styles.js
@@ -6,14 +6,41 @@
  */
 
 import IconAtom from 'atoms/Icon';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import Color from 'utils/color';
 
-export const Wrapper = styled(Link)`
+export const Wrapper = styled.div`
   display: inline-flex;
   justify-content: center;
   align-content: center;
   align-items: center;
+  position: relative;
+  z-index: 0;
+
+  &::before {
+    content: '';
+    display: block;
+    background-color: ${Color.pink.fade(0.1).string()};
+    height: 3px;
+    width: 0%;
+    transition: all 0.3s ease;
+    opacity: 0;
+    position: absolute;
+    bottom: 8px;
+    left: 0;
+    z-index: -1;
+  }
+
+  &:hover {
+    &::before {
+      width: 100%;
+      opacity: 1;
+    }
+  }
+
+  a {
+    text-decoration: none !important;
+  }
 
   p {
     margin-bottom: 0 !important;
