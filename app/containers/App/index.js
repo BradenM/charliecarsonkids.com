0d22db6ff282b0feae46e2259a88ec9c08de3f02
Braden Mars <bradenmars@bradenmars.me>
2020-02-23 17:46:33 -0600

    feat: Refresh auth token on reload

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -7,17 +7,45 @@
  *
  */
 
+import { useMutation } from '@apollo/react-hooks';
+import { VerifyToken } from 'api/mutations/auth.graphql';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import CatalogPage from 'pages/CatalogPage/loadable';
 import HomePage from 'pages/HomePage/loadable';
 import Login from 'pages/Login/loadable';
 import ProductPage from 'pages/ProductPage/loadable';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomeLayout from 'templates/HomeLayout';
 import GlobalStyle from '../../global-styles';
+import { useStore } from '../../store';
 
-export default function App() {
+const App = () => {
+  const [verifyToken] = useMutation(VerifyToken);
+  const [, dispatch] = useStore();
+
+  const refreshAuth = async () => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    if (typeof token === 'string') {
+      const options = { variables: { token } };
+      const { errors } = await verifyToken(options);
+      if (errors) {
+        return dispatch({
+          type: 'userLogout',
+        });
+      }
+      return dispatch({
+        type: 'userLogin',
+        payload: token,
+      });
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    refreshAuth();
+  }, []);
+
   return (
     <>
       <GlobalStyle />
@@ -32,4 +60,6 @@ export default function App() {
       </HomeLayout>
     </>
   );
-}
+};
+
+export default App;
